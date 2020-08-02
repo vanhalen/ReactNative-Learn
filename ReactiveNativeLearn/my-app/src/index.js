0bbd811b3d906cc8014b16f06971d07753c1c7d6
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2020-08-02 18:21:50 -0300

    chore: commit inicial para digoflix

--- ReactiveNativeLearn/my-app/src/index.js
+++ ReactiveNativeLearn/my-app/src/index.js
@@ -3,31 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-
-	constructor(props){
-		//Todos os componentes de classe react que possuem um construtor devem ser inicializados com o super
-		super(props);
-		this.state = {
-			value: null,
-		}
-	}
-
 	render() {
 		return (
 		// isso ()=>alert('UEEEEPA!') é o mesmo que isso function(){alert('UEEEEPA!')}
 		<button
 			className="square"
-			onClick={ ()=>this.setState({value: 'X'}) }
+			onClick={ ()=>this.props.onClick() }
 		>
-			{this.state.value}
+			{this.props.value}
 		</button>
 		);
 	}
 }
 
 class Board extends React.Component {
+	/** */
+	constructor(props){
+		//Todos os componentes de classe react que possuem um construtor devem ser inicializados com o super
+		super(props);
+		this.state = {
+			square: Array(9).fill(null),
+		}
+	}
+/**/
+	handleClick(i){
+		const squares = this.state.squares.slice();
+		squares[i] = 'X';
+		this.setState({squares: squares});
+	}
+
 	renderSquare(i) {
-		return <Square value={i}/>;
+		return <Square value={this.state.square[i]} onClick={()=>this.handleClicl(i)}/>;
 	}
 
 	render() {
