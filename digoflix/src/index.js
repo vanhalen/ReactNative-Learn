c15965913afe7ac3201bf02a0fd139565c4a1c82
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2020-08-03 23:52:22 -0300

    chore: removido app.js + criação da pasta Page contendo as páginas do projeto

--- digoflix/src/index.js
+++ digoflix/src/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./pages/home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/home";
+import CadastroVideo from "./pages/cadastro/video";
+
+//Desafio master blaster na descrição
+const Pagina404 = () => <div>Página não encontrada: 404</div>;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      {/* exact  - Barra de ir para home caso o usuário digite uma url inexistente */}
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
+//Esse arquivo é o root, aqui declaramos nosso APP e setamos as rotas pelo react-router-dom
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
---
+++ digoflix/src/pages/cadastro/video/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CadastroVideo() {
+  return <div>Página de Cadastro de Vídeo</div>;
+}
+
+export default CadastroVideo;
