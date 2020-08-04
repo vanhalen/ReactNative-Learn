86b8029d9841412ca77c88044b71998f06c853e0
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2020-08-03 23:56:59 -0300

    feat: adicionado SPA com react-router-dom para mudar de tela sem carregar a página

--- digoflix/src/components/Menu/index.js
+++ digoflix/src/components/Menu/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "../../assets/img/logo.png";
 import "./Menu.css";
 import Button from "../Button";
+import { Link } from "react-router-dom";
 // import ButtonLink from "./ButtonLink";
 
 function Menu() {
@@ -12,7 +13,8 @@ function Menu() {
       </a>
 
       {/* as="a" o button vai se comportar como uma tag a */}
-      <Button as="a" className="ButtonLink" href="/">
+      {/* <Button as="a" className="ButtonLink" href="/cadastro/video"> */}
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </nav>
