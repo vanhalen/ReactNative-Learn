0a63eeff2852e1ee09c59df73ea39302e2f2441c
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2020-08-03 22:56:34 -0300

    chore: ajuste rodapé

--- digoflix/src/App.js
+++ digoflix/src/App.js
@@ -12,13 +12,15 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é chupiknévis? Como vivem?"}
+        videoDescription={"Assista agora um dos melhores guitarristas"}
       />
 
       <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
       <Carousel category={dadosIniciais.categorias[1]} />
       <Carousel category={dadosIniciais.categorias[2]} />
       <Carousel category={dadosIniciais.categorias[3]} />
+      <Carousel category={dadosIniciais.categorias[4]} />
+      <Carousel category={dadosIniciais.categorias[5]} />
 
       <Footer />
     </div>
