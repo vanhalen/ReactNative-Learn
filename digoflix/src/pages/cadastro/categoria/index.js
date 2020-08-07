75a5fe2764a38fd5d13e98b66471bc575ca41747
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2020-08-07 17:25:05 -0300

    fix: correção de bug no form + comentários

--- digoflix/src/pages/cadastro/categoria/index.js
+++ digoflix/src/pages/cadastro/categoria/index.js
@@ -55,47 +55,76 @@ const ButtonSucces = withStyles({
 })(Button);
 
 function CadastroCategoria() {
+  const values = {
+    titulo: "",
+    link_extra: "",
+    cor: "#ff0000",
+  };
   //Utilizando set para edição de variável
   const [categorias, setCategorias] = useState(["TesteArray"]);
-  const [nomeDaCategoria, setNomeDaCategoria] = useState("");
+  const [form, setForm] = useState(values);
+
+  /**Seta os valores para o form */
+  function setValue(key, value) {
+    setForm({
+      ...form,
+      [key]: value,
+    });
+  }
+
+  /** Salva o estado do form quando o usuário alterar o valor*/
+  function saveState(infosEvento) {
+    // const { getAttribute, value } = infosEvento.target;
+    // setValue(getAttribute("name"), value);
+    setValue(infosEvento.target.getAttribute("name"), infosEvento.target.value);
+  }
+
+  /** Faz Submit do form */
+  function handleSubmit(infosDoEvento) {
+    infosDoEvento.preventDefault();
+    setCategorias([...categorias, form]);
+    setValue({ values });
+  }
 
   return (
     <PageDefault>
       {/* <h1>Cadastro de Categoria</h1> */}
-      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
-      <form
-        onSubmit={function handleSubmit(infosDoEvento) {
-          infosDoEvento.preventDefault();
-          setCategorias([...categorias, nomeDaCategoria]);
-        }}
-      >
+      <h1>Cadastro de Categoria: {form.titulo}</h1>
+      <form onSubmit={handleSubmit}>
         <TextFieldWhite
-          id="titulo"
           label="Nome da Categoria:"
           variant="outlined"
           autoComplete="off"
-          value={nomeDaCategoria}
+          value={form.titulo}
+          name="titulo"
           //Função para salvar o estado - Pode ter qualquer nome
-          onChange={function saveState(infosEvento) {
-            setNomeDaCategoria(infosEvento.target.value);
-          }}
+          onChange={saveState}
           fullWidth
         />
         <TextFieldWhite
           id="link_extra"
           label="Link Extra:"
+          value={form.link_extra}
           variant="outlined"
           autoComplete="off"
+          name="link_extra"
+          onChange={saveState}
           fullWidth
         />
-        Cor: <Input type="color" fullWidth />
+        Cor:{" "}
+        <Input
+          type="color"
+          value={form.cor}
+          name="cor"
+          onChange={saveState}
+          fullWidth
+        />
         <ul>
           {categorias.map((categoria, indice) => {
             // return <li key={`${categoria}${indice}`}>{categoria}</li>; //É a mesma coisa
-            return <li key={categoria + indice}>{categoria}</li>;
+            return <li key={categoria + indice}>{categoria.titulo}</li>;
           })}
         </ul>
-        <Button>cadastrar</Button>
         <Box display="flex" justifyContent="flex-end">
           <ButtonSucces variant="outlined" color="primary" type="submit">
             Cadastrar
