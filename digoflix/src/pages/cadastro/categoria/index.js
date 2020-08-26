868535b11c394aa93b2a016b7d0e8b33f9933c6b
Rodrigo Chagas <rodrigorrchagas@gmail.com>
2020-08-25 23:14:51 -0300

    feat: adicionado tabela customizada listando categorias

--- digoflix/src/pages/cadastro/categoria/index.js
+++ digoflix/src/pages/cadastro/categoria/index.js
@@ -1,16 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Box from '@material-ui/core/Box';
-import PageDefault from '../../../components/PageDefault';
-import FormField from '../../../components/FormField';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { CircularProgress } from '@material-ui/core';
 import FormButton from '../../../components/FormButton';
+import FormField from '../../../components/FormField';
+import PageDefault from '../../../components/PageDefault';
 
 const URL_BACK = 'http://localhost:6060/';
 
 function CadastroCategoria() {
   const values = {
     titulo: '',
-    link_extra: '',
+    link_extra_nome: '',
+    link_extra_url: '',
     descricao: '',
     cor: '#ff0000',
   };
@@ -45,7 +55,7 @@ function CadastroCategoria() {
       },
       body: JSON.stringify({
         titulo: form.titulo,
-        link_extra: form.link_extra,
+        link_extra: { text: form.link_extra_nome, url: form.link_extra_url },
         cor: form.cor,
         descricao: form.descricao,
       }),
@@ -72,6 +82,31 @@ function CadastroCategoria() {
     });
   }, []);
 
+  const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: '#9e9e9e',
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+  const classes = useStyles();
+
   return (
     <PageDefault>
       {/* <h1>Cadastro de Categoria</h1> */}
@@ -89,12 +124,25 @@ function CadastroCategoria() {
           onChange={saveState}
           required
         />
-        <FormField
-          name="link_extra"
-          label="Link Extra:"
-          value={form.link_extra}
-          onChange={saveState}
-        />
+        <Box width="45%" display="inline-block" marginRight="1%">
+
+          <FormField
+            name="link_extra_nome"
+            label="Link Extra - Nome:"
+            value={form.link_extra_nome}
+            onChange={saveState}
+          />
+        </Box>
+
+        <Box width="54%" display="inline-block">
+          <FormField
+            name="link_extra_url"
+            label="Link Extra - URL:"
+            value={form.link_extra_url}
+            onChange={saveState}
+          />
+        </Box>
+
         <FormField
           name="descricao"
           label="Descrição:"
@@ -102,6 +150,7 @@ function CadastroCategoria() {
           onChange={saveState}
           multiline
         />
+
         <FormField
           type="color"
           label="Cor:"
@@ -116,16 +165,41 @@ function CadastroCategoria() {
       </form>
 
       {/* Aparece se não existir categorias */}
-      {categorias.length === 0 && <div>Carregando...</div>}
+      {/* {categorias.length > 0 && <CircularProgress />} */}
 
-      <ul>
-        {categorias.map((categoria, indice) => (
-          // return <li key={`${categoria}${indice}`}>{categoria}</li>; //É a mesma coisa
-          <li key={categoria + indice}>{categoria.titulo}</li>
-        ))}
-      </ul>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="left">Nome</StyledTableCell>
+              <StyledTableCell align="left">Link Extra- Nome</StyledTableCell>
+              <StyledTableCell align="left">Descrição</StyledTableCell>
+              <StyledTableCell align="center">Cor</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {categorias.map((row) => (
+              <StyledTableRow key={row.titulo}>
+                <StyledTableCell component="th" scope="row">
+                  {row.titulo}
+                </StyledTableCell>
+                <StyledTableCell>{row.link_extra ? row.link_extra.text : ''}</StyledTableCell>
+                <StyledTableCell>{row.descricao}</StyledTableCell>
+                <StyledTableCell align="center">
+                  <input
+                    type="color"
+                    label=""
+                    value={row.cor}
+                    disabled
+                  />
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
 
-      <Link to="/">Ir para home</Link>
+      {/* <Link to="/">Ir para home</Link> */}
     </PageDefault>
   );
 }
